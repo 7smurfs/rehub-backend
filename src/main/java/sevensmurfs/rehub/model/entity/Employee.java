c0c1c63fdfb72340eb08bac96e131303e741008c
begJosip <josipbegic57@gmail.com>
2023-10-20 04:36:28 +0200

    RH-18 Create employee model and schema

---
+++ src/main/java/sevensmurfs/rehub/model/entity/Employee.java
@@ -0,0 +1,50 @@
+package sevensmurfs.rehub.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(uniqueConstraints = @UniqueConstraint(name = "uc_employee_pin", columnNames = "pin"))
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String pin;
+
+    @Column(nullable = false)
+    private String profession;
+
+    @Column(nullable = false)
+    private LocalDateTime dateOfBirth;
+
+    private String gender;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private RehubUser user;
+
+}
