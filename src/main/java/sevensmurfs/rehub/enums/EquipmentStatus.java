2104baed6172d9e3c70293d82a11c7ea3a00fedf
begJosip <josipbegic57@gmail.com>
2023-10-21 16:02:19 +0200

    RH-13 Create room and equipment model and schema

--- src/main/java/sevensmurfs/rehub/enums/EquipmentStatus.java
+++ src/main/java/sevensmurfs/rehub/enums/EquipmentStatus.java
@@ -1,4 +1,20 @@
 package sevensmurfs.rehub.enums;
 
 public enum EquipmentStatus {
+
+    /**
+     * equipment not working needs repair
+     */
+    OUT_OF_SERVICE,
+
+    /**
+     * equipment is operable and ready for use
+     */
+    OPERABLE,
+
+    IN_USE,
+
+    FREE_FOR_USE
+    ;
+
 }
