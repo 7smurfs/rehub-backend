5e53b44a5c24fce362f54d2059fe08209622e8d2
ErikGreblo <erik.greblo@fer.hr>
2023-10-31 12:20:12 +0100

    RH-29-Endpoint-for-creating-therapy-for-patient

--- src/main/java/sevensmurfs/rehub/repository/DoctorRepository.java
+++ src/main/java/sevensmurfs/rehub/repository/DoctorRepository.java
@@ -1,10 +1,20 @@
 package sevensmurfs.rehub.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import sevensmurfs.rehub.enums.Role;
 import sevensmurfs.rehub.model.entity.Doctor;
+import sevensmurfs.rehub.model.entity.Room;
+import sevensmurfs.rehub.model.entity.UserRole;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
 
+    @Query("SELECT d FROM Doctor d WHERE d.firstName = :firstName AND d.lastName = :lastName")
+    Doctor findByFirstAndLastName(@Param("firstName") String firstName, @Param("lastName") String lastName);
 }
