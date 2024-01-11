542dcfe7a1d7e8966ef4d66d726ae83dc12d0c5e
begJosip <josipbegic57@gmail.com>
2024-01-11 19:26:34 +0100

    Add patient full name to therapy result response message

--- src/main/java/sevensmurfs/rehub/model/message/response/TherapyResultResponse.java
+++ src/main/java/sevensmurfs/rehub/model/message/response/TherapyResultResponse.java
@@ -16,11 +16,15 @@ public class TherapyResultResponse {
 
     private String status;
 
+    private String patientName;
+
     public static TherapyResultResponse mapTherapyResultEntity(TherapyResult therapyResult) {
         return TherapyResultResponse.builder()
                                     .id(therapyResult.getId())
                                     .therapyId(therapyResult.getTherapy().getId())
                                     .result(therapyResult.getResult())
+                                    .patientName(therapyResult.getTherapy().getPatient().getFirstName() + " " +
+                                                 therapyResult.getTherapy().getPatient().getLastName())
                                     .status(therapyResult.getStatus())
                                     .build();
     }
